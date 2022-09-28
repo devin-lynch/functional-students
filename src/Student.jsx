0c6c893535fd6e98cc1dc854bdd511722117680d
devin-lynch <devin.l.lynch@gmail.com>
2022-09-28 14:28:27 -0500

    Refactor map and add comment

--- src/Student.jsx
+++ src/Student.jsx
@@ -2,22 +2,32 @@ import Score from './Score'
 
 
 function Student(props) {
-    const scoreComponents = props.scores.map((element, i) => {
-        return (
-            <Score
-                score={element.score}
-                date={element.date}
-                key={`score-${i}`}
-            />
-        )
-    })
+    // const scoreComponents = props.scores.map((element, i) => {
+    //     return (
+    //         <Score
+    //             score={element.score}
+    //             date={element.date}
+    //             key={`score-${i}`}
+    //         />
+    //     )
+    // })
     return (
         <div>
             <h1>{props.name}</h1>
 
             <p>{props.bio}</p>
 
-            {scoreComponents}
+            {/* you can put the js directly into the div and not have to create a variable/call to it! */}
+            {props.scores.map((element, i) => {
+                return (
+                    <Score
+                        score={element.score}
+                        date={element.date}
+                        key={`score-${i}`}
+                    />
+                 )
+            })}
+            {/* {scoreComponents} */}
         </div>
     )
 }
